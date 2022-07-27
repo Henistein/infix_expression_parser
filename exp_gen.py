a092cefc8fb203e8fa4231e1f5dd2b440b19f0fb
Henistein <henriquejesus20@gmail.com>
2022-07-27 01:34:01 +0100

    mitigate overflow and recursion issues

--- exp_gen.py
+++ exp_gen.py
@@ -1,6 +1,9 @@
-import random
-import sys
-sys.setrecursionlimit(10000)
+import numpy.random as random
+from tqdm import trange
+
+# TODO: Fix recursion issues
+# TODO: Fix overflow issues
+# TODO: Fix cast issues
 
 class ExpGen:
   # generate an expression X
@@ -9,17 +12,24 @@ class ExpGen:
   # for each term generate a number or an expression
   # example: A1 -> 1 or A1 -> (X)
   # for each _ choose a random operator (+, -, *, /, ^)
-  def __init__(self, max_terms=3, max_number=10):
+  def __init__(self, max_terms=10, max_number=10):
     self.operators = ['+', '-', '*', '/', '**']
-    self.max_terms = max_terms
+    self.max_terms = max_terms + 1
+    self.max_terms_back = max_terms + 1
     self.max_number = max_number
   
-  def generate(self):
+  def _generate(self):
+    # probability of 0.70 to decrease the number of terms
+    if random.random() < 0.7:
+      self.max_terms -= 1
+      if self.max_terms < 2:
+        self.max_terms = 2
+      
     n_terms = random.randint(1, self.max_terms)
-    terms = '_'.join([str(random.choice([random.randint(0, self.max_number), 'X'])) for _ in range(n_terms)])
+    terms = '_'.join([str(random.choice([random.randint(1, self.max_number), 'X'], p=[0.6, 0.4])) for _ in range(n_terms)])
 
     # swap all the _ with random operators
-    terms = [random.choice(self.operators) if elem == '_' else elem for elem in terms]
+    terms = [random.choice(self.operators, p=[0.24, 0.24, 0.24, 0.24, 0.04]) if elem == '_' else elem for elem in terms]
 
     if 'X' not in terms:
       return ''.join(terms)
@@ -28,11 +38,29 @@ class ExpGen:
       indexes = [i for i, elem in enumerate(terms) if elem == 'X']
       for i in indexes:
         # replace X with a random operator
-        terms[i] = '(' + self.generate() + ')'
+        terms[i] = '(' + self._generate() + ')'
       return ''.join(terms)
+  
+  def generate(self, n_expressions=1):
+    expressions = []
+    for _ in trange(n_expressions):
+      tries = 0
+      self.max_terms = self.max_terms_back
+      while True:
+        if tries == 100:
+          print('100 tries failed, returning (0, 0)...')
+          return (0, 0)
+        try:
+          exp = self._generate()
+          res = eval(exp)
+          break
+        except (ZeroDivisionError, OverflowError):
+          tries += 1
+          exp = self._generate()
+      expressions.append((exp, res))
+    return expressions
       
   
 
 if __name__ == '__main__':
-  for i in range(1000):
-    print(ExpGen().generate())
+  print(ExpGen().generate(1000))
