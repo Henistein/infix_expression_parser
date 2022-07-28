e61523fc74c5f6337652e063cf886a640372cd3e
Henistein <henriquejesus20@gmail.com>
2022-07-28 20:52:09 +0100

    optimization: from 0.47 to 0.35 ms with 100 terms

--- solver.py
+++ solver.py
@@ -29,37 +29,10 @@ def perform_op(exp, idx_1, idx_2, op):
 
   return exp
 
-def ret_index(lst, val):
-  try:
-    return lst.index(val)
-  except ValueError:
-    return -1
-
 def solve(exp):
   exp = decompose(exp)
-  return _solve(exp)
-
-def old_solve(exp):
-  exp = decompose(exp)
   return _solve_(exp)
 
-#@profile
-def _solve_(exp):
-  if '+' not in exp and '*' not in exp and '/' not in exp:
-    assert len(exp) == 1, 'Invalid expression:  %s' % exp
-    return float(exp[0])
-
-  if '*' in exp or '/' in exp:
-    op_idx_times = ret_index(exp, '*')
-    op_idx_div = ret_index(exp, '/')
-    if op_idx_div == -1 or (op_idx_times < op_idx_div and op_idx_times != -1):
-      exp = perform_op(exp, op_idx_times-1, op_idx_times+1, exp[op_idx_times])
-    else:
-      exp = perform_op(exp, op_idx_div-1, op_idx_div+1, exp[op_idx_div])
-  else:
-    exp = perform_op(exp, ret_index(exp, '+')-1, ret_index(exp, '+')+1, '+')
-
-  return _solve_(exp)
 
 def _solve(exp):
 
@@ -82,20 +55,29 @@ def _solve(exp):
 
   return _solve(exp)
 
+def _solve_(exp):
+
+  if len(exp) == 1:
+    return float(exp[0])
+  if len(exp) == 3:
+    return float(perform_op(exp, 0, 2, exp[1])[0])
+  
+  curr_op = exp[1]
+  next_op = exp[3]
+  if next_op in ['*', '/'] and curr_op not in ['*', '/']:
+    exp = perform_op(exp, 2, 4, next_op)
+  else:
+    exp = perform_op(exp, 0, 2, curr_op)
+    
+  return _solve_(exp)
+
+
       
 from exp_gen import ExpGen
 from not_mine import evaluate
 from not_mine2 import Solution
 
 expressions = ExpGen(max_terms=100, parenthesis_prob=0, max_number=100, seed=420).generate(10000)
-
-print('Old Mine:')
-start = time.time()
-for pair in expressions:
-  solved = old_solve(pair[0])
-  if round(solved, 3) != round(pair[1], 3):
-    print("Expression: %s\nOutput: %s\nExpected: %s" % (pair[0], solved, pair[1]))
-print("Time: %s" % (time.time() - start))
 
 print('Mine:')
 start = time.time()
